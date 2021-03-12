45edc0a6ed8a19ccab684ad3b80d37ac90484d28
menezessn <mar_chelo10@hotmail.com>
2021-03-11 21:18:03 -0300

    PHP

---
+++ projeto.php
@@ -0,0 +1,25 @@
+<?php
+
+function imprimirValor(){
+    echo "<h1>Valor impresso!!!!</h1>";
+}
+
+//executar função
+imprimirValor();
+
+
+function somaValor($valor1, $valor2){
+
+    return($valor1+$valor2);
+
+}
+
+echo("soma de valores :" .$resultado  = somaValor(20,15));
+
+printf("</br> Valor a ser pago R$ %.2f", $resultado);
+
+
+
+
+
+?>
